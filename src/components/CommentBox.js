d4b5aa479417219cc08c3be45a7ef39f9c378ba2
Rafael Do Nascimento <rafael.nascimento@RJ-MB3818.local>
2019-05-20 22:54:24 -0300

    Changing CommentBox to be a class component and adding textarea and button components.

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CommentBox = () => {
-    return <div>I'm the CommentBox component!</div>
-} 
+class CommentBox extends Component {
+    state = { comment: '' };
+
+    handleChange = event => {
+        this.setState({ comment: event.target.value });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        
+        this.setState({comment: '' });
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h4>Add a comment</h4>
+                <textarea value={this.state.comment} onChange={this.handleChange} />
+                <div>
+                    <button>Submit comment</button>
+                </div>
+            </form>
+        );
+    }
+}
 
 export default CommentBox;
