1c20fa1e09ed2347089c9bb6120c70c8db4474a4
Rafael Do Nascimento <rafael.nascimento@olxbr.com>
2019-05-26 21:40:00 -0300

    Adding CommentBox tests.

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+	//It's not necessary to use mount here. I could use shallow, since there is no children component,
+	//but Stephen wanted to show us how to use mount.
+	wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+	wrapped.unmount();
+});
+
+describe('CommentBox component', () => {
+	it('shows textarea and button elements ', () => {
+		expect(wrapped.find('textarea').length).toEqual(1);
+		expect(wrapped.find('button').length).toEqual(1);
+	});
+
+	describe('the text area', () => {
+		beforeEach(() => {
+			wrapped.find('textarea').simulate('change', {
+				target: { value: 'new comment' }
+			});
+	
+			wrapped.update();
+		});
+
+		it('receives user inputs', () => {
+			expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+		});
+	
+		it('is cleared after form submission', () => {
+			expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+	
+			wrapped.find('form').simulate('submit');
+	
+			wrapped.update();
+	
+			expect(wrapped.find('textarea').prop('value')).toEqual('');
+		});
+	});	
+});
