06fa7ee5f615005cc477cbe6bd06eca5ca3e5819
Rafael Do Nascimento <rafael.nascimento@olxbr.com>
2019-05-29 11:06:56 -0300

    Adicionando teste do comment reducer.

---
+++ src/reducers/__tests__/comments.test.js
@@ -0,0 +1,13 @@
+import commentReducer from 'reducers/comments';
+import { SAVE_COMMENT } from 'actions/types';
+
+describe('Comment reducer should', () => {
+    it('save the comment', () => {
+        const action = { type: SAVE_COMMENT, payload: 'natchos blatchos' };
+        
+        const changedState = commentReducer([], action);
+
+        expect(changedState.length).toEqual(1)
+        expect(changedState[0]).toEqual('natchos blatchos');
+    })
+})
